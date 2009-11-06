4cec50d3ba00a1a7afc1fc6f4ce536fe9759170f
Joshua Tolley <josh@endpoint.com>
2009-11-06 02:01:02 -0700

    Add simple function to interact with pgsql APIs, and make something for the NQP bits to export

--- src/plparrot.c
+++ src/plparrot.c
@@ -10,10 +10,19 @@
 
 PG_MODULE_MAGIC;
 
+Datum plparrot_call_handler(PG_FUNCTION_ARGS);
+void plparrot_elog(int level, char *message);
+
 PG_FUNCTION_INFO_V1(plparrot_call_handler);
 
 Datum
 plparrot_call_handler(PG_FUNCTION_ARGS)
 {
     PG_RETURN_VOID();
+}
+
+void
+plparrot_elog(int level, char *message)
+{
+    elog(level, "%s", message);
 }
