e6a12207bd3bcef8a147a6264068c741dc17c8a2
Duke Leto <jonathan@leto.net>
2009-11-15 13:57:54 -0800

    example test script that creates the plparrot language and creates a void function

--- t/test.sql
+++ t/test.sql
@@ -1,3 +1,11 @@
-DROP FUNCTION plparrot_call_handler() CASCADE;
-CREATE FUNCTION plparrot_call_handler() RETURNS LANGUAGE_HANDLER AS 'plparrot' LANGUAGE C;
+
+--DROP FUNCTION plparrot_call_handler() CASCADE;
+begin;
+
+CREATE FUNCTION plparrot_call_handler() RETURNS LANGUAGE_HANDLER AS '/Users/leto/git/postgresql/contrib/plparrot/libplparrot.so' LANGUAGE C;
 CREATE LANGUAGE plparrot HANDLER plparrot_call_handler;
+CREATE FUNCTION test_parrot() RETURNS void AS $$ FAIL $$ LANGUAGE plparrot;
+
+select test_parrot();
+rollback;
+
