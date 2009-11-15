561630c66568dee51efa099ced6ed7f4e02aacbe
Duke Leto <jonathan@leto.net>
2009-11-15 14:57:59 -0800

    Basic sanity test for plparrot

--- t/test.sql
+++ t/test.sql
@@ -4,8 +4,23 @@ begin;
 
 CREATE FUNCTION plparrot_call_handler() RETURNS LANGUAGE_HANDLER AS '/Users/leto/git/postgresql/contrib/plparrot/libplparrot.so' LANGUAGE C;
 CREATE LANGUAGE plparrot HANDLER plparrot_call_handler;
-CREATE FUNCTION test_parrot() RETURNS void AS $$ FAIL $$ LANGUAGE plparrot;
 
-select test_parrot();
+CREATE FUNCTION test_void() RETURNS void AS $$ FAIL $$ LANGUAGE plparrot;
+
+CREATE FUNCTION test_int() RETURNS int AS $$ 1 $$ LANGUAGE plparrot;
+
+CREATE FUNCTION test_int_int(int) RETURNS int AS $$ $1 $$ LANGUAGE plparrot;
+
+CREATE FUNCTION test_float() RETURNS float AS $$ 1.0 $$ LANGUAGE plparrot;
+
+
+select test_void();
+select test_int();
+-- this returns 0 still
+select test_int_int(42);
+
+select test_float();
+
+
 rollback;
 
