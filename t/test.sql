f3687f4d7ec77ece4e1d35b81a6c936151596453
Duke Leto <jonathan@leto.net>
2009-11-22 23:38:32 -0800

    Add a tweaked pgtap.sql that works on my setup
    Some failing tests which require basic type marshalling to work

--- t/test.sql
+++ t/test.sql
@@ -1,28 +1,22 @@
 \set ECHO
 \set QUIET 1
 -- Turn off echo and keep things quiet.
-
 -- Format the output for nice TAP.
 \pset format unaligned
 \pset tuples_only true
 \pset pager
-
 -- Revert all changes on failure.
 \set ON_ERROR_ROLLBACK 1
 \set ON_ERROR_STOP true
 \set QUIET 1
-
 -- Load the TAP functions.
 BEGIN;
     \i pgtap.sql
-
 -- Plan the tests.
-SELECT plan(1);
-
+SELECT plan(2);
 
 --DROP FUNCTION plparrot_call_handler() CASCADE;
-begin;
-
+-- TODO: Make this configurable
 CREATE FUNCTION plparrot_call_handler() RETURNS LANGUAGE_HANDLER AS '/Users/leto/git/postgresql/contrib/plparrot/libplparrot.so' LANGUAGE C;
 CREATE LANGUAGE plparrot HANDLER plparrot_call_handler;
 
@@ -30,19 +24,15 @@ CREATE FUNCTION test_void() RETURNS void AS $$ FAIL $$ LANGUAGE plparrot;
 
 CREATE FUNCTION test_int() RETURNS int AS $$ 1 $$ LANGUAGE plparrot;
 
-
 CREATE FUNCTION test_int_int(int) RETURNS int AS $$ $1 $$ LANGUAGE plparrot;
 
 CREATE FUNCTION test_float() RETURNS float AS $$ 1.0 $$ LANGUAGE plparrot;
 
 CREATE FUNCTION test_varchar() RETURNS varchar(5) AS $$ 'cheese' $$ LANGUAGE plparrot;
 
-
 select test_void();
-select test_int();
--- this returns 0 still
-select test_int_int(42);
-
+select is(test_int(),1);
+select is(test_int_int(42),42);
 -- these give a bus error on darwin/x86 + Postgres 8.3.8
 --select test_float();
 --select test_varchar();
@@ -51,4 +41,3 @@ select test_int_int(42);
 SELECT * FROM finish();
 
 rollback;
-
