4e7500d5a967ddc946c2395f2a1a3e955943eecf
Duke Leto <jonathan@leto.net>
2010-06-29 00:34:08 -0700

    Add a test for returning strings from PL/Perl6

--- t/sql/plperl6.sql
+++ t/sql/plperl6.sql
@@ -15,7 +15,7 @@ BEGIN;
 \i plparrot.sql
 
 -- Plan the tests.
-SELECT plan(3);
+SELECT plan(4);
 
 CREATE FUNCTION test_void_plperl6(integer) RETURNS void AS $$ Nil $$ LANGUAGE plperl6;
 
@@ -23,10 +23,12 @@ CREATE FUNCTION test_int_plperl6(integer) RETURNS int AS $$ 42 $$ LANGUAGE plper
 
 CREATE FUNCTION test_float_plperl6(integer) RETURNS float AS $$ 5.0 $$ LANGUAGE plperl6;
 
+CREATE FUNCTION test_string_plperl6() RETURNS varchar AS $$ "rakudo" $$ LANGUAGE plperl6;
 
 select is(test_int_plperl6(89),42,'Return an integer from PL/Perl6');
 select is(test_void_plperl6(42)::text,''::text,'Return nothing from PL/Perl6');
 select is(test_float_plperl6(2), 5.0::float,'Return a float from PL/Perl6');
+select is(test_string_plperl6(), 'rakudo','Return a varchar from PL/Perl6');
 
 -- Finish the tests and clean up.
 SELECT * FROM finish();
